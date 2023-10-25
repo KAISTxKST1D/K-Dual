4a6da8931258abfcf667f4f81d06324516c750b2
minizzang <minizzang@kaist.ac.kr>
2023-10-26 01:11:17 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "K-Dual"
+include(":mobile")
+include(":wear")
