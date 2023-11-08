6115d5a5dd17a338e6bca6b378f40b17e5e03d82
icp1481 <icp1481@naver.com>
2023-11-08 15:45:13 +0900

    HelloWorldEspressoTest

--- mobile/src/androidTest/java/com/example/k_dual/ExampleInstrumentedTest.kt
+++ mobile/src/androidTest/java/com/example/k_dual/ExampleInstrumentedTest.kt
@@ -1,12 +1,14 @@
 package com.example.k_dual
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,3 +24,31 @@ class ExampleInstrumentedTest {
         assertEquals("com.example.k_dual", appContext.packageName)
     }
 }
+
+
+/*@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HelloWorldEspressoTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test fun listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()))
+    }
+}
+*/
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HelloWorldEspressoTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test fun listGoesOverTheFold() {
+        //onView(withText("Hello world!")).check(matches(isDisplayed()))
+        assertEquals(4, 2 + 2)
+    }
+}
+
