9221cf59f80da06a266e9839f82e481f824a59ef
kws <kws1207@kaist.ac.kr>
2023-11-11 23:32:53 +0900

    Apply suggested changes from lint report

--- mobile/src/main/java/com/example/k_dual/ui/theme/Theme.kt
+++ mobile/src/main/java/com/example/k_dual/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 fun KDualTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+//    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
 //    val colorScheme = when {
