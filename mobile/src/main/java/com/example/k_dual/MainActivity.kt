320681fddd80618ae34ef2f02f50792fa0c7fc95
Sam <60031762+kws1207@users.noreply.github.com>
2023-11-12 21:45:48 +0900

    Add mobile app UI (#4)
    * Change mobile to compose, add typography and UI components
    
    * Add home screen, WIP in user screen
    
    * Finish user screen UI except popups
    
    * Add TextFieldAlertDialog component
    
    * Implement dialogs of user screen
    
    * Add user color screen
    
    * Add alert screen
    
    * Change file names to uppercase
    
    * Implement low/high value setting dialogs into alert screen
    
    * Add unit screen
    
    * Add BackButtonTitleRow component
    
    * Apply self review
    
    * Remove unnecessary change
    
    * Apply suggested changes from lint report
    
    * Fix fonts
    
    * Apply reviews

--- mobile/src/main/java/com/example/k_dual/MainActivity.kt
+++ mobile/src/main/java/com/example/k_dual/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.k_dual
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.k_dual.screen.AlertScreen
+import com.example.k_dual.screen.ColorScreen
+import com.example.k_dual.screen.HomeScreen
+import com.example.k_dual.screen.UnitScreen
+import com.example.k_dual.screen.UserScreen
+import com.example.k_dual.ui.theme.KDualTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            KDualTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeContentPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        modifier = Modifier,
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
+                        composable("home") {
+                            HomeScreen(navController)
+                        }
+                        composable("unit") {
+                            UnitScreen(navController)
+                        }
+                        composable(
+                            "user/{index}",
+                            arguments = listOf(
+                                navArgument("index") {
+                                    type = NavType.IntType
+                                }
+                            ),
+                        ) {backStackEntry ->
+                            UserScreen(navController, backStackEntry.arguments?.getInt("index") == 1)
+                        }
+                        composable(
+                            "user/{index}/color",
+                            arguments = listOf(
+                                navArgument("index") {
+                                    type = NavType.IntType
+                                }
+                            ),
+                        ) {backStackEntry ->
+                            ColorScreen(navController, backStackEntry.arguments?.getInt("index") == 1)
+                        }
+                        composable(
+                            "user/{index}/alert",
+                            arguments = listOf(
+                                navArgument("index") {
+                                    type = NavType.IntType
+                                }
+                            ),
+                        ) {backStackEntry ->
+                            AlertScreen(navController, backStackEntry.arguments?.getInt("index") == 1)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
