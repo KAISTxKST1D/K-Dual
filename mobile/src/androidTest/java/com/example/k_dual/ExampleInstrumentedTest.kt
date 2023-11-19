58b4296ef782ad1e239311978cd4562a3cb95be8
icp1481 <icp1481@naver.com>
2023-11-19 22:03:10 +0900

    del ExampleInstrumentedTest

--- mobile/src/androidTest/java/com/example/k_dual/ExampleInstrumentedTest.kt
+++ mobile/src/androidTest/java/com/example/k_dual/ExampleInstrumentedTest.kt
@@ -1,21 +1,19 @@
 package com.example.k_dual
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-/*@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
@@ -24,46 +22,3 @@ class ExampleInstrumentedTest {
         assertEquals("com.example.k_dual", appContext.packageName)
     }
 }
-
-
-/*@RunWith(AndroidJUnit4::class)
-@LargeTest
-class HelloWorldEspressoTest {
-
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
-    @Test fun listGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()))
-    }
-}
-*/
-
-@RunWith(AndroidJUnit4::class)
-@LargeTest
-class HelloWorldEspressoTest {
-
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
-    @Test fun listGoesOverTheFold() {
-        //onView(withText("Hello world!")).check(matches(isDisplayed()))
-        assertEquals(4, 2 + 3)
-    }
-}
-
-*/
-
-/*
-internal class GitHubActionTest {
-    @Test
-    fun testSuccess() {
-        assert(true)
-    }
-
-    @Test
-    fun testFail() {
-        assert(false)
-    }
-}
-*/
