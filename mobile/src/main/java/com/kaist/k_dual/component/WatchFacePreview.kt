fc55d43d82bdac0518717abe4e7c1384bdd70ca1
minizzang <minizzang@kaist.ac.kr>
2023-11-26 22:30:08 +0900

    Chore: mobile preview dummy data

--- mobile/src/main/java/com/kaist/k_dual/component/WatchFacePreview.kt
+++ mobile/src/main/java/com/kaist/k_dual/component/WatchFacePreview.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kaist.k_canvas.KCanvas
-import com.kaist.k_canvas.KColor
 import com.kaist.k_dual.model.ManageSetting
 import com.kaist.k_dual.ui.theme.KDualTheme
 
@@ -66,11 +65,11 @@ fun WatchFacePreview(
                         1,
                         isFirstHighLight,
                         firstUserColor,
-                        "4",
+                        "0",
                         ManageSetting.settings.glucoseUnits,
                         robotoRegular
                     )
-                    KCanvas.drawBloodGlucose(canvas, 1, "144", ManageSetting.settings.glucoseUnits, robotoMedium)
+                    KCanvas.drawBloodGlucose(canvas, 1, "000", ManageSetting.settings.glucoseUnits, robotoMedium)
                     KCanvas.drawBackgroundBox(canvas, "down", isSecondHighLight, secondUserColor)
                     KCanvas.drawIconAndUserName(
                         canvas,
@@ -85,11 +84,11 @@ fun WatchFacePreview(
                         2,
                         isSecondHighLight,
                         secondUserColor,
-                        "4",
+                        "0",
                         ManageSetting.settings.glucoseUnits,
                         robotoRegular
                     )
-                    KCanvas.drawBloodGlucose(canvas, 2, "144", ManageSetting.settings.glucoseUnits, robotoMedium)
+                    KCanvas.drawBloodGlucose(canvas, 2, "000", ManageSetting.settings.glucoseUnits, robotoMedium)
                 } else {
                     KCanvas.drawBackgroundBox(canvas, null, isFirstHighLight, firstUserColor)
                     KCanvas.drawIconAndUserName(
@@ -99,18 +98,17 @@ fun WatchFacePreview(
                         firstUserColor,
                         robotoMedium
                     )
-                    KCanvas.drawBloodGlucose(canvas, null, "144", ManageSetting.settings.glucoseUnits, robotoMedium)
+                    KCanvas.drawBloodGlucose(canvas, null, "000", ManageSetting.settings.glucoseUnits, robotoMedium)
                     KCanvas.drawDiffArrowBox(
                         canvas,
                         context,
                         null,
                         isFirstHighLight,
                         firstUserColor,
-                        "4",
+                        "0",
                         ManageSetting.settings.glucoseUnits,
                         robotoRegular
                     )
-
                 }
             }
         }
