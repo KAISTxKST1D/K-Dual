911964d7ae7c4767628b191fdbf96a762456370b
kws <kws1207@kaist.ac.kr>
2023-11-26 14:52:36 +0900

    WIP 2

--- mobile/src/main/java/com/kaist/k_dual/ui/theme/Type.kt
+++ mobile/src/main/java/com/kaist/k_dual/ui/theme/Type.kt
@@ -1,12 +1,20 @@
 package com.kaist.k_dual.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kaist.k_dual.R
 
@@ -22,7 +30,7 @@ val robotoFontFamily = FontFamily(
     Font(googleFont = robotoFontName, fontProvider = provider)
 )
 val notoSansFontFamily =  FontFamily(
-    Font(R.font.notosanskr_regular, FontWeight.Normal),
+    Font(R.font.notosanscjkkr_vf, FontWeight.Normal),
     Font(R.font.notosanskr_medium, FontWeight.Medium),
 )
 
@@ -103,3 +111,30 @@ val KoreanTypography = Typography(
         fontWeight = FontWeight.Medium,
     ),
 )
+
+@Composable
+fun TypographyPreview() {
+    Column {
+        Text("Headline Small", style = EnglishTypography.headlineSmall)
+        Text("Title Large", style = EnglishTypography.titleLarge)
+        Text("Title Small", style = EnglishTypography.titleSmall)
+        Text("Body Large", style = EnglishTypography.bodyLarge)
+        Text("Body Medium", style = EnglishTypography.bodyMedium)
+        Text("Label Large", style = EnglishTypography.labelLarge)
+
+        Spacer(Modifier.height(16.dp))
+
+        Text("머리글 작게", style = KoreanTypography.headlineSmall)
+        Text("제목 크게", style = KoreanTypography.titleLarge)
+        Text("제목 작게", style = KoreanTypography.titleSmall)
+        Text("본문 크게", style = KoreanTypography.bodyLarge)
+        Text("본문 중간", style = KoreanTypography.bodyMedium)
+        Text("레이블 크게", style = KoreanTypography.labelLarge)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTypography() {
+    TypographyPreview()
+}
