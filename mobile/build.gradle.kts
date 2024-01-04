ade76a45342fd49a7e2ec616c5aaacb4c3b61615
Minhee Kim <79495204+minizzang@users.noreply.github.com>
2024-01-04 18:43:56 +0900

    Add crashlytics in mobile (#33)

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -65,6 +67,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
     implementation(project(mapOf("path" to ":K-canvas")))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
